5a2de8f5e552d8a196dd3f7adcb5d820b4e3a1b0
Tom Morgan <tom.r.morgan@gmail.com>
2021-02-27 06:37:34 +0000

    Create client.js

---
+++ Day5/client.js
@@ -0,0 +1,48 @@
+import { CallClient, CallAgent } from "@azure/communication-calling";
+import { AzureCommunicationTokenCredential } from '@azure/communication-common';
+
+const connectButton = document.getElementById('connect-button');
+const disconnectButton = document.getElementById('disconnect-button');
+const callStateElement = document.getElementById('call-state');
+const destinationUserElement = document.getElementById('destination-user-input');
+
+let call;
+let callAgent;
+let callClient;
+
+async function init() {
+    
+  callClient = new CallClient();
+	
+  //get an access token to use
+  const response = await fetch('YOUR ACS TOKEN ISSUING WEB FUNCTION URL HERE (WITH THE CODE). SEE DAY 3');
+  const responseJson = await response.json(); 
+  const token = responseJson.token;
+  const tokenCredential = new AzureCommunicationTokenCredential(token);
+  callAgent = await callClient.createCallAgent(tokenCredential);
+  connectButton.disabled = false;
+}
+init();
+
+connectButton.addEventListener("click", () => {        	
+    const destinationToCall = { communicationUserId: destinationUserElement.value};
+	call = callAgent.startCall([destinationToCall]);
+    
+    call.on('callStateChanged', () => {
+        callStateElement.innerText = call.state;
+    })
+	
+    // toggle button states
+    disconnectButton.disabled = false;
+    connectButton.disabled = true;
+});
+
+disconnectButton.addEventListener("click", async () => {
+    await call.hangUp();
+  
+    // toggle button states
+    disconnectButton.disabled = true;
+    connectButton.disabled = false;
+    callStateElement.innerText = '-';
+  });
+
